dd8aa58d8626736e04c399fd5a7b1bcaf4d5006a
artapresyan <apresyanarturv@gmail.com>
2022-09-27 20:57:24 +0400

    added customer entity

---
+++ src/main/java/com/example/YerevanCinema/entities/Customer.java
@@ -0,0 +1,101 @@
+package com.example.YerevanCinema.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long customerID;
+
+    @Column(name = "name", nullable = false)
+    private String customerName;
+
+    @Column(name = "surname", nullable = false)
+    private String customerSurname;
+
+    @Column(name = "age", nullable = false)
+    private Integer customerAge;
+
+    @Column(name = "username", nullable = false)
+    private String customerUsername;
+
+    @Column(name = "email", nullable = false)
+    private String customerEmail;
+
+    @Column(name = "password", nullable = false)
+    private String customerPassword;
+
+    public Customer() {
+    }
+
+    public Customer(Long customerID, String customerName, String customerSurname,
+                    Integer customerAge, String customerUsername, String customerEmail, String customerPassword) {
+        this.customerID = customerID;
+        this.customerName = customerName;
+        this.customerSurname = customerSurname;
+        this.customerAge = customerAge;
+        this.customerUsername = customerUsername;
+        this.customerEmail = customerEmail;
+        this.customerPassword = customerPassword;
+    }
+
+    public Long getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Long customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerSurname() {
+        return customerSurname;
+    }
+
+    public void setCustomerSurname(String customerSurname) {
+        this.customerSurname = customerSurname;
+    }
+
+    public Integer getCustomerAge() {
+        return customerAge;
+    }
+
+    public void setCustomerAge(Integer customerAge) {
+        this.customerAge = customerAge;
+    }
+
+    public String getCustomerUsername() {
+        return customerUsername;
+    }
+
+    public void setCustomerUsername(String customerUsername) {
+        this.customerUsername = customerUsername;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPassword() {
+        return customerPassword;
+    }
+
+    public void setCustomerPassword(String customerPassword) {
+        this.customerPassword = customerPassword;
+    }
+}

9211c895f5c6b10e42cc52a6a2915eede9c45424
artapresyan <apresyanarturv@gmail.com>
2022-09-27 22:05:16 +0400

    getting customer by id

--- src/main/java/com/example/YerevanCinema/services/CustomerService.java
+++ src/main/java/com/example/YerevanCinema/services/CustomerService.java
@@ -1,8 +1,13 @@
 package com.example.YerevanCinema.services;
 
+import com.example.YerevanCinema.entities.Customer;
+import com.example.YerevanCinema.exceptions.NoSuchCustomerException;
 import com.example.YerevanCinema.repositories.CustomerRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -14,5 +19,14 @@ public class CustomerService {
         this.customerRepository = customerRepository;
         this.passwordEncoder = passwordEncoder;
     }
+
+    public Customer getCustomerByID(Long customerID) throws NoSuchCustomerException {
+        Optional<Customer> customer = customerRepository.findById(customerID);
+        if (customer.isPresent()) {
+            return customer.get();
+        } else
+            throw new NoSuchCustomerException(String.format("No customer with %s id", customerID));
+    }
+
 
 }

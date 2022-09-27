a8df38cacd26142ad99b608bbd3dacaf09f4f96c
artapresyan <apresyanarturv@gmail.com>
2022-09-27 22:23:42 +0400

    added custom exception for duplicate emails

---
+++ src/main/java/com/example/YerevanCinema/exceptions/RegisteredEmailException.java
@@ -0,0 +1,8 @@
+package com.example.YerevanCinema.exceptions;
+
+public class RegisteredEmailException extends Exception{
+
+    public RegisteredEmailException(String message){
+        super(message);
+    }
+}

e8c3a52854502061a1e4a189882a950161658ecb
artapresyan <apresyanarturv@gmail.com>
2022-09-27 22:37:06 +0400

    added data validation method

--- src/main/java/com/example/YerevanCinema/services/CustomerService.java
+++ src/main/java/com/example/YerevanCinema/services/CustomerService.java
@@ -2,6 +2,8 @@ package com.example.YerevanCinema.services;
 
 import com.example.YerevanCinema.entities.Customer;
 import com.example.YerevanCinema.exceptions.NoSuchCustomerException;
+import com.example.YerevanCinema.exceptions.RegisteredEmailException;
+import com.example.YerevanCinema.exceptions.UsernameExistsException;
 import com.example.YerevanCinema.repositories.CustomerRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -33,4 +35,26 @@ public class CustomerService {
     }
 
 
+
+    private void validateData(String customerName, String customerSurname, Integer customerAge,
+                              String customerUsername, String customerEmail, String customerPassword)
+            throws UsernameExistsException, NullPointerException, RegisteredEmailException {
+        if (customerName == null) {
+            throw new NullPointerException("Name is missing, fill it in");
+        } else if (customerSurname == null) {
+            throw new NullPointerException("Surname is missing, fill it in");
+        } else if (customerAge == null) {
+            throw new NullPointerException("Age is missing, fill it in");
+        } else if (customerUsername == null) {
+            throw new NullPointerException("Username is missing, fill it in");
+        } else if (customerRepository.getByCustomerUsername(customerUsername) != null) {
+            throw new UsernameExistsException("Username already exists. Try to get another one");
+        } else if (customerEmail == null) {
+            throw new NullPointerException("Email is missing, fill it in");
+        } else if (customerRepository.getByCustomerEmail(customerEmail) != null) {
+            throw new RegisteredEmailException("Email already registered. Try to get another one");
+        } else if (customerPassword == null) {
+            throw new NullPointerException("Password is missing, fill it in");
+        }
+    }
 }

75260b01b1c47e634496a9a8e4920d6f0f5ea841
artapresyan <apresyanarturv@gmail.com>
2022-09-27 19:55:07 +0400

    initial commit

---
+++ src/main/java/com/example/YerevanCinema/YerevanCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.YerevanCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YerevanCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YerevanCinemaApplication.class, args);
+	}
+
+}

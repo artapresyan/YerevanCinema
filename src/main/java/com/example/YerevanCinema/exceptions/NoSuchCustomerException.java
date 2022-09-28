e9ed64850efe82e3ad5f13f1405c39fa15881c7d
artapresyan <apresyanarturv@gmail.com>
2022-09-28 14:28:19 +0400

    changing exception name to more meaningful

--- src/main/java/com/example/YerevanCinema/exceptions/NoSuchCustomerException.java
+++
@@ -1,7 +0,0 @@
-package com.example.YerevanCinema.exceptions;
-
-public class NoSuchCustomerException extends Exception{
-    public NoSuchCustomerException(String message){
-        super(message);
-    }
-}

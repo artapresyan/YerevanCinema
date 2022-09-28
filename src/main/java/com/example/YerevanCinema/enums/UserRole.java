69a0f329251c8bdd3d9da7e4582b9751f6680b78
artapresyan <apresyanarturv@gmail.com>
2022-09-28 23:10:39 +0400

    added enum for roles

---
+++ src/main/java/com/example/YerevanCinema/enums/UserRole.java
@@ -0,0 +1,31 @@
+package com.example.YerevanCinema.enums;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.example.YerevanCinema.enums.UserPermission.*;
+
+public enum UserRole {
+    CUSTOMER(Set.of(READ)),
+    ADMIN(Set.of(READ, UPDATE)),
+    SUPERADMIN(Set.of(CREATE, READ, UPDATE, DELETE));
+
+    private final Set<UserPermission> permissions;
+
+    UserRole(Set<UserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<UserPermission> getPermissions(){
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities(){
+        Set<SimpleGrantedAuthority> authorities = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission())).collect(Collectors.toSet());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
+}

412d3a482ea2236e7deb409c0f6d32f6d3cb41b0
artapresyan <apresyanarturv@gmail.com>
2022-09-28 14:48:14 +0400

    added registration method for admins

--- src/main/java/com/example/YerevanCinema/services/AdminService.java
+++ src/main/java/com/example/YerevanCinema/services/AdminService.java
@@ -1,16 +1,70 @@
 package com.example.YerevanCinema.services;
 
+import com.example.YerevanCinema.entities.Admin;
+import com.example.YerevanCinema.entities.Customer;
+import com.example.YerevanCinema.exceptions.NoSuchUserException;
+import com.example.YerevanCinema.exceptions.RegisteredEmailException;
+import com.example.YerevanCinema.exceptions.UsernameExistsException;
 import com.example.YerevanCinema.repositories.AdminRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminService {
 
-    private AdminRepository adminRepository;
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AdminService(AdminRepository adminRepository) {
+    public AdminService(AdminRepository adminRepository, PasswordEncoder passwordEncoder) {
         this.adminRepository = adminRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    public Admin getAdminByID(Long adminID) throws NoSuchUserException {
+        Optional<Admin> admin = adminRepository.findById(adminID);
+        if (admin.isPresent()) {
+            return admin.get();
+        } else
+            throw new NoSuchUserException(String.format("No admin with %s id", admin));
+    }
 
+    public List<Admin> getAllAdmins() {
+        return adminRepository.findAll();
+    }
+
+    public Admin registerAdmin(String adminName, String adminSurname, String adminEmail, String adminUsername,
+                               String adminPassword) {
+        try {
+            validateData(adminName, adminSurname, adminEmail, adminUsername, adminPassword);
+            Admin admin = new Admin(adminName, adminSurname, adminEmail, adminUsername, adminPassword);
+            adminRepository.save(admin);
+            return admin;
+        } catch (NullPointerException | UsernameExistsException | RegisteredEmailException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void validateData(String adminName, String adminSurname, String adminEmail, String adminUsername,
+                              String adminPassword)
+            throws UsernameExistsException, NullPointerException, RegisteredEmailException {
+        if (adminName == null) {
+            throw new NullPointerException("Name is missing, fill it in");
+        } else if (adminSurname == null) {
+            throw new NullPointerException("Surname is missing, fill it in");
+        } else if (adminUsername == null) {
+            throw new NullPointerException("Username is missing, fill it in");
+        } else if (adminRepository.getByAdminUsername(adminUsername) != null) {
+            throw new UsernameExistsException("Username already exists. Try to get another one");
+        } else if (adminEmail == null) {
+            throw new NullPointerException("Email is missing, fill it in");
+        } else if (adminRepository.getByAdminEmail(adminEmail) != null) {
+            throw new RegisteredEmailException("Email already registered. Try to get another one");
+        } else if (adminPassword == null) {
+            throw new NullPointerException("Password is missing, fill it in");
+        }
+    }
 }

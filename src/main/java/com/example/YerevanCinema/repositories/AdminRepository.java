8b883d836037ce8656156c0b21b83399f9622182
artapresyan <apresyanarturv@gmail.com>
2022-09-28 14:55:42 +0400

    fixing return type

--- src/main/java/com/example/YerevanCinema/repositories/AdminRepository.java
+++ src/main/java/com/example/YerevanCinema/repositories/AdminRepository.java
@@ -1,11 +1,10 @@
 package com.example.YerevanCinema.repositories;
 
 import com.example.YerevanCinema.entities.Admin;
-import com.example.YerevanCinema.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
-    Customer getByAdminUsername(String customerUsername);
-    Customer getByAdminEmail(String customerEmail);
+    Admin getByAdminUsername(String customerUsername);
+    Admin getByAdminEmail(String customerEmail);
 }

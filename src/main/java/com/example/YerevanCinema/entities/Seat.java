5e9d2abb3e25864275c7dcb5395e91b7310c589a
artapresyan <apresyanarturv@gmail.com>
2022-09-30 02:07:39 +0400

    added seat entity

---
+++ src/main/java/com/example/YerevanCinema/entities/Seat.java
@@ -0,0 +1,40 @@
+package com.example.YerevanCinema.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "seats")
+@Getter @Setter
+@NoArgsConstructor
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long seatID;
+
+    @Column(name = "line")
+    @NotNull
+    private Integer seatRow;
+
+    @Column(name = "number")
+    @NotNull
+    private Integer seatNumber;
+
+    @Column(name = "sold")
+    @NotNull
+    private Boolean isSold;
+
+
+
+    public Seat(Integer seatRow, Integer seatNumber, Boolean isSold) {
+        this.seatRow = seatRow;
+        this.seatNumber = seatNumber;
+        this.isSold = isSold;
+    }
+}

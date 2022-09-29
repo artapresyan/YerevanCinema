2aaba8b5b46fc3c5182086ff538813f8c7f6f4ba
artapresyan <apresyanarturv@gmail.com>
2022-09-29 23:18:51 +0400

    fixing return type

--- src/main/java/com/example/YerevanCinema/repositories/MovieRepository.java
+++ src/main/java/com/example/YerevanCinema/repositories/MovieRepository.java
@@ -3,8 +3,10 @@ package com.example.YerevanCinema.repositories;
 import com.example.YerevanCinema.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     Movie getByMovieName(String movieName);
-    Movie getByMovieCategory(String movieCategory);
+    List<Movie> getByMovieCategory(String movieCategory);
 }

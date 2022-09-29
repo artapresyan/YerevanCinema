ca87504e9ad45b816a3d4d2ea118794c69e37da0
artapresyan <apresyanarturv@gmail.com>
2022-09-29 23:06:13 +0400

    optimizing imports and code

--- src/main/java/com/example/YerevanCinema/YerevanCinemaApplication.java
+++ src/main/java/com/example/YerevanCinema/YerevanCinemaApplication.java
@@ -2,14 +2,6 @@ package com.example.YerevanCinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.BufferedImageHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.awt.image.BufferedImage;
-import java.util.Properties;
 
 @SpringBootApplication
 public class YerevanCinemaApplication {
@@ -18,20 +10,4 @@ public class YerevanCinemaApplication {
 		SpringApplication.run(YerevanCinemaApplication.class, args);
 	}
 
-	@Bean JavaMailSender javaMailSender(){
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost("smtp.gmail.com");
-		mailSender.setPort(587);
-
-		mailSender.setUsername("cinemayerevan@gmail.com");
-		mailSender.setPassword("yerevan123456");
-
-		Properties props = mailSender.getJavaMailProperties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.debug", "true");
-
-		return mailSender;
-	}
 }

5695ec685c5636f1675e38a9a9eed87fd014a67d
artapresyan <apresyanarturv@gmail.com>
2022-09-30 00:32:26 +0400

    added custom exception for non existing halls

---
+++ src/main/java/com/example/YerevanCinema/exceptions/HallNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.YerevanCinema.exceptions;
+
+public class HallNotFoundException extends Exception{
+
+    public HallNotFoundException(String message){
+        super(message);
+    }
+}

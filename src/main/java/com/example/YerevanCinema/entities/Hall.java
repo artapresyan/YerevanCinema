b567bf28a7d0146ac1b22fdeffbdac098b390e76
artapresyan <apresyanarturv@gmail.com>
2022-09-30 00:30:56 +0400

    added hall entity

---
+++ src/main/java/com/example/YerevanCinema/entities/Hall.java
@@ -0,0 +1,34 @@
+package com.example.YerevanCinema.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "halls")
+@Getter @Setter
+@NoArgsConstructor
+public class Hall {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long hallID;
+
+    @Column(name = "name")
+    @NotBlank
+    private String hallName;
+
+    @Column(name = "capacity")
+    @NonNull
+    private Integer hallCapacity;
+
+    public Hall(String hallName, @NonNull Integer hallCapacity) {
+        this.hallName = hallName;
+        this.hallCapacity = hallCapacity;
+    }
+}

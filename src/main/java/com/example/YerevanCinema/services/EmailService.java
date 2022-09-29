a66d577968150779ca6f132cba783b0dfcf3f893
artapresyan <apresyanarturv@gmail.com>
2022-09-29 23:43:10 +0400

    added interface for email service

--- src/main/java/com/example/YerevanCinema/services/EmailService.java
+++ src/main/java/com/example/YerevanCinema/services/EmailService.java
@@ -1,34 +1,12 @@
 package com.example.YerevanCinema.services;
 
 import com.example.YerevanCinema.entities.Customer;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.io.File;
 
-@Service
-public class EmailService {
+public interface EmailService {
 
-    private final JavaMailSender emailSender;
+    void sendMessageWithAttachment(Customer customer, String pathToAttachment) throws MessagingException;
 
-    public EmailService(JavaMailSender emailSender) {
-        this.emailSender = emailSender;
-    }
-
-    public void sendMessageWithAttachment(Customer customer, String pathToAttachment) throws MessagingException {
-        MimeMessage message = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setTo(customer.getCustomerEmail());
-        helper.setSubject("Ticket");
-        helper.setText("Scan QR attached to this mail");
-
-        FileSystemResource file
-                = new FileSystemResource(new File(pathToAttachment));
-        helper.addAttachment("QR", file.getFile());
-        emailSender.send(message);
-    }
+    void sendSimpleMessage(Customer customer,String text, String subject) throws MessagingException;
 }

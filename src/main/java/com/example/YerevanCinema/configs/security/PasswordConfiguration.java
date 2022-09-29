a592ebf3c61f63c36a85f6dfc812a996acdeb847
artapresyan <apresyanarturv@gmail.com>
2022-09-29 23:05:19 +0400

    packaging

--- src/main/java/com/example/YerevanCinema/configs/security/PasswordConfiguration.java
+++ src/main/java/com/example/YerevanCinema/configs/security/PasswordConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.YerevanCinema.security;
+package com.example.YerevanCinema.configs.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

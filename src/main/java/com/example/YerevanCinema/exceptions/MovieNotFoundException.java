25e627782f20e9512122d247e400cde57dc56fca
artapresyan <apresyanarturv@gmail.com>
2022-09-29 22:18:49 +0400

    added custom exception for movies

---
+++ src/main/java/com/example/YerevanCinema/exceptions/MovieNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.YerevanCinema.exceptions;
+
+public class MovieNotFoundException extends Exception{
+
+    public MovieNotFoundException(String message){
+        super(message);
+    }
+}

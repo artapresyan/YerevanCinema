8dbc94baec247ce021154f828399496608807603
artapresyan <apresyanarturv@gmail.com>
2022-09-30 17:42:21 +0400

    added custom exception for non existing tickets

---
+++ src/main/java/com/example/YerevanCinema/exceptions/TicketNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.YerevanCinema.exceptions;
+
+public class TicketNotFoundException extends Exception{
+
+    public TicketNotFoundException(String message){
+        super(message);
+    }
+}

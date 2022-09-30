ebaf448fe9e4d87b732835592f7fee3ed49c6bc8
artapresyan <apresyanarturv@gmail.com>
2022-09-30 11:32:35 +0400

    added seat repository

---
+++ src/main/java/com/example/YerevanCinema/repositories/SeatRepository.java
@@ -0,0 +1,8 @@
+package com.example.YerevanCinema.repositories;
+
+import com.example.YerevanCinema.entities.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+}

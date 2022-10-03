0759f777b9f34c4296156e82ee1671311619abc9
artapresyan <apresyanarturv@gmail.com>
2022-10-03 19:35:26 +0400

    implementing get customer home page method

--- src/main/java/com/example/YerevanCinema/controllers/CustomerController.java
+++ src/main/java/com/example/YerevanCinema/controllers/CustomerController.java
@@ -1,15 +1,21 @@
 package com.example.YerevanCinema.controllers;
 
+import com.example.YerevanCinema.entities.Customer;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/customer/")
 public class CustomerController {
 
-    @GetMapping("home")
-    private String getCustomerHomePage() {
+    @GetMapping
+    private String getCustomerHomePage(HttpSession session, Model model) {
+        Customer customer = (Customer) session.getAttribute("user");
+        model.addAttribute("user", customer);
         return "main_view";
     }
 

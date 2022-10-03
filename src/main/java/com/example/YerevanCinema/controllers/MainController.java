192e85c3d1fde92203f5e536d906aad70ce41424
artapresyan <apresyanarturv@gmail.com>
2022-10-03 13:43:27 +0400

    added main controller

--- src/main/java/com/example/YerevanCinema/controllers/MainController.java
+++ src/main/java/com/example/YerevanCinema/controllers/MainController.java
@@ -1,12 +1,20 @@
 package com.example.YerevanCinema.controllers;
 
+import com.example.YerevanCinema.entities.Customer;
+import com.example.YerevanCinema.services.implementations.CustomerServiceImpl;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+
+    private final CustomerServiceImpl customerService;
+
+    public MainController(CustomerServiceImpl customerService) {
+        this.customerService = customerService;
+    }
 
     @GetMapping
     public String getMainPage() {
@@ -21,6 +29,21 @@ public class MainController {
     @GetMapping("signup")
     public String getSighUpPage() {
         return "signup_view";
+    }
+
+    @PostMapping("signup")
+    public String signUpCustomer(@RequestParam("name") String name, @RequestParam("surname") String surname,
+                                 @RequestParam("age") Integer age, @RequestParam("email") String email,
+                                 @RequestParam("username") String username, @RequestParam("password") String password,
+                                 @RequestParam("confirm_password") String confirmPassword, Model model) {
+        if (customerService.confirmPassword(password, confirmPassword)) {
+            Customer customer = customerService.registerCustomer(name, surname, age, username, email, password);
+            if (customer != null) {
+                model.addAttribute("customer", customer);
+                return "main_view";
+            }
+        }
+        return "login_view";
     }
 
     @GetMapping("about")
@@ -39,7 +62,7 @@ public class MainController {
     }
 
     @GetMapping("recover")
-    public String getRecoverPage(){
+    public String getRecoverPage() {
         return "recover_view";
     }
 }

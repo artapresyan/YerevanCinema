800b7149e6d698e741491e864adc344625cc3076
artapresyan <apresyanarturv@gmail.com>
2022-10-03 13:46:43 +0400

    added admin controller

---
+++ src/main/java/com/example/YerevanCinema/controllers/AdminController.java
@@ -0,0 +1,12 @@
+package com.example.YerevanCinema.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+
+
+}

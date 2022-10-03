9a96b70c02ce967f813e7d434d2b944b576152c1
artapresyan <apresyanarturv@gmail.com>
2022-10-03 20:46:26 +0400

    added method

--- src/main/java/com/example/YerevanCinema/controllers/CustomerController.java
+++ src/main/java/com/example/YerevanCinema/controllers/CustomerController.java
@@ -1,8 +1,11 @@
 package com.example.YerevanCinema.controllers;
 
 import com.example.YerevanCinema.entities.Customer;
-import com.example.YerevanCinema.services.implementations.CustomerServiceImpl;
+import com.example.YerevanCinema.entities.MovieSession;
+import com.example.YerevanCinema.entities.Seat;
+import com.example.YerevanCinema.exceptions.MovieSessionNotFoundException;
 import com.example.YerevanCinema.services.implementations.GmailClientServiceImpl;
+import com.example.YerevanCinema.services.implementations.MovieSessionServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,17 +15,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/customer/")
 public class CustomerController {
 
-    private final CustomerServiceImpl customerService;
+    private final MovieSessionServiceImpl movieSessionService;
 
     private final GmailClientServiceImpl gmailClientService;
 
-    public CustomerController(CustomerServiceImpl customerService, GmailClientServiceImpl gmailClientService) {
-        this.customerService = customerService;
+    public CustomerController(MovieSessionServiceImpl movieSessionService, GmailClientServiceImpl gmailClientService) {
+        this.movieSessionService = movieSessionService;
         this.gmailClientService = gmailClientService;
     }
 
@@ -58,18 +64,44 @@ public class CustomerController {
         return "main_view";
     }
 
+    @GetMapping("details")
+    public String getAccountDetails(HttpSession session, Model model) {
+        Customer customer = (Customer) session.getAttribute("user");
+        model.addAttribute("user", customer);
+        return "customer_details_view";
+    }
+
+    @GetMapping("tickets")
+    public String getPurchasedTickets(HttpSession session, Model model) {
+        Customer customer = (Customer) session.getAttribute("user");
+        model.addAttribute("user", customer);
+        return "tickets_view";
+    }
+
     @GetMapping("sessions")
-    public String getSessionsPage() {
+    public String getSessionsPage(HttpSession session, Model model) {
+        Customer customer = (Customer) session.getAttribute("user");
+        List<MovieSession> sessions = movieSessionService.getAllMovieSessions().stream().filter(movieSession ->
+                        movieSession.getMovieSessionStart().getDayOfMonth() == LocalDateTime.now().getDayOfMonth())
+                .collect(Collectors.toList());
+        model.addAttribute("user", customer);
+        model.addAttribute("sessions", sessions);
         return "sessions_view";
     }
 
-    @GetMapping("details")
-    public String getDetailsPage() {
-        return "details_view";
-    }
-
-    @GetMapping("sessions/seat")
-    public String getSeatsPage() {
-        return "seat_view";
+    @GetMapping("/sessions/seats")
+    public String getSessionSeats(HttpSession session, @RequestParam("movieSessionID") Long movieSessionID,
+                                  Model model) {
+        Customer customer = (Customer) session.getAttribute("user");
+        try {
+            MovieSession movieSession = movieSessionService.getMovieSessionByID(movieSessionID);
+            List<Seat> seats = movieSession.getHall().getHallSeats().stream()
+                    .filter(seat -> !seat.getIsSold()).collect(Collectors.toList());
+            model.addAttribute("user", customer);
+            model.addAttribute("seats", seats);
+            return "session_seats_view";
+        } catch (MovieSessionNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

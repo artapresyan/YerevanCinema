c781b7e8b55b7f42c5de9fa5b1fb568f37b4352f
artapresyan <apresyanarturv@gmail.com>
2022-10-05 01:56:06 +0400

    making methods to return mime message

--- src/main/java/com/example/YerevanCinema/services/EmailService.java
+++ src/main/java/com/example/YerevanCinema/services/EmailService.java
@@ -3,12 +3,13 @@ package com.example.YerevanCinema.services;
 import com.example.YerevanCinema.entities.Customer;
 
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 public interface EmailService {
 
-    void sendMessageWithAttachment(Customer customer, String pathToAttachment) throws MessagingException;
+    MimeMessage sendMessageWithAttachment(Customer customer, String pathToAttachment) throws MessagingException;
 
-    void sendSimpleMessage(Customer customer,String text, String subject) throws MessagingException;
+    MimeMessage sendSimpleMessage(Customer customer,String text, String subject) throws MessagingException;
 
-    void getSimpleMessage(String email, String text) throws MessagingException;
+    MimeMessage getSimpleMessage(String email, String text) throws MessagingException;
 }

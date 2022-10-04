b9f905dc803e9d219b2040bf1124e000b266ac38
artapresyan <apresyanarturv@gmail.com>
2022-10-04 13:24:24 +0400

    added customer details service

---
+++ src/main/java/com/example/YerevanCinema/services/detailsServices/CustomerDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.YerevanCinema.services.detailsServices;
+
+import com.example.YerevanCinema.entities.entityDetails.CustomerDetails;
+import com.example.YerevanCinema.exceptions.UserNotFoundException;
+import com.example.YerevanCinema.services.implementations.CustomerServiceImpl;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomerDetailsService implements UserDetailsService {
+
+    private final CustomerServiceImpl customerService;
+
+    public CustomerDetailsService(CustomerServiceImpl customerService) {
+        this.customerService = customerService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            return new CustomerDetails(customerService.getCustomerByUsername(username));
+        } catch (UserNotFoundException e) {
+            return null;
+        }
+    }
+}

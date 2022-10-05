898b79f562e2fecb492225c1fb3fa4e6ae58ab5b
artapresyan <apresyanarturv@gmail.com>
2022-10-05 10:58:43 +0400

    added method to get all tickets

--- src/main/java/com/example/YerevanCinema/services/TicketService.java
+++ src/main/java/com/example/YerevanCinema/services/TicketService.java
@@ -5,8 +5,11 @@ import com.example.YerevanCinema.entities.MovieSession;
 import com.example.YerevanCinema.entities.Ticket;
 import com.example.YerevanCinema.exceptions.TicketNotFoundException;
 
+import java.util.List;
+
 public interface TicketService {
 
+    List<Ticket> getAllTickets();
     Ticket getTicketByID(Long ticketID) throws TicketNotFoundException;
 
     Ticket addTicket(Customer customer, MovieSession movieSession);

bd73254aafe35342396dae19bc9de317e6ea680c
artapresyan <apresyanarturv@gmail.com>
2022-10-06 23:16:41 +0400

    implementing user validation methods

--- src/main/java/com/example/YerevanCinema/services/validations/AdminValidationService.java
+++ src/main/java/com/example/YerevanCinema/services/validations/AdminValidationService.java
@@ -3,7 +3,7 @@ package com.example.YerevanCinema.services.validations;
 import com.example.YerevanCinema.exceptions.RegisteredEmailException;
 import com.example.YerevanCinema.exceptions.UsernameExistsException;
 import com.example.YerevanCinema.repositories.AdminRepository;
-import com.example.YerevanCinema.repositories.CustomerRepository;
+import com.example.YerevanCinema.services.UserValidationService;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,31 +12,30 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 
 @Service
-public class UserValidationService {
+public class AdminValidationService implements UserValidationService {
 
     private final AdminRepository adminRepository;
-    private final CustomerRepository customerRepository;
     private final Logger logger = LogManager.getLogger();
 
-    public UserValidationService(AdminRepository adminRepository, CustomerRepository customerRepository) {
+    public AdminValidationService(AdminRepository adminRepository) {
         this.adminRepository = adminRepository;
-        this.customerRepository = customerRepository;
     }
 
+    @Override
     public void validateName(String name) throws IOException {
         if (name == null || !name.matches("^[A-Z][a-z]{2,20}$")) {
             logger.log(Level.FATAL, String.format("Please be aware, name ' %s ' is invalid ", name));
             throw new IOException();
         }
     }
-
+    @Override
     public void validateSurname(String surname) throws IOException {
         if (surname == null || !surname.matches("^[A-Z][a-z]{2,30}")) {
             logger.log(Level.FATAL, String.format("Please be aware,surname ' %s ' is invalid ", surname));
             throw new IOException();
         }
     }
-
+    @Override
     public void validateAge(Integer age) throws IOException {
         if (age == null || age < 16) {
             logger.log(Level.FATAL, String.format("Please be aware, age ' %s ' is invalid or restricted by our system",
@@ -45,32 +44,30 @@ public class UserValidationService {
         }
 
     }
-
+    @Override
     public void validateUsername(String username) throws IOException, UsernameExistsException {
         if (username == null || !username.matches("^(?=.{3,}[a-z])[a-z0-9]{8,30}$")) {
             logger.log(Level.FATAL, String.format("Please be aware, username ' %s ' is invalid", username));
             throw new IOException();
-        } else if (customerRepository.getByCustomerUsername(username) != null
-                || adminRepository.getByAdminUsername(username) != null) {
+        } else if (adminRepository.getByAdminUsername(username) != null) {
             logger.log(Level.ERROR, String.format("Username ' %s ' already exists, try to get another one", username));
             throw new UsernameExistsException(String.format("Username ' %s ' exists", username));
         }
     }
-
+    @Override
     public void validateEmail(String email) throws IOException, RegisteredEmailException {
         if (email == null || !email.matches("^[a-z][a-z0-9-_.]+[a-z0-9]+@[a-z]+\\.[a-z.]{2,}")) {
             logger.log(Level.FATAL, String.format("Please be aware, email ' %s ' is invalid", email));
             throw new IOException();
-        } else if (customerRepository.getByCustomerEmail(email) != null
-                || adminRepository.getByAdminEmail(email) != null) {
+        } else if (adminRepository.getByAdminEmail(email) != null) {
             logger.log(Level.ERROR, String.format("Email ' %s ' already registered in system, try with another one", email));
             throw new RegisteredEmailException(String.format("Email ' %s ' already registered", email));
         }
     }
-
-    public void validatePassword(String password) throws IOException{
+    @Override
+    public void validatePassword(String password) throws IOException {
         if (password == null || !password
-                .matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%#&()/*?])[A-Za-z\\d@$!%#&()/*?]{8,20}$")){
+                .matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%#&()/*?])[A-Za-z\\d@$!%#&()/*?]{8,20}$")) {
             logger.log(Level.FATAL, "Please be aware, password must contain at least" +
                     " one special character, one capital letter, one number, and the minimum length is 10 characters");
             throw new IOException();

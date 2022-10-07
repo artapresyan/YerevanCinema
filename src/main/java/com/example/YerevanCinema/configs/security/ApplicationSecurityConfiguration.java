ff58c26f70b832ebeb81453559d362f5e63e8f35
artapresyan <apresyanarturv@gmail.com>
2022-10-07 20:53:23 +0400

    configuring jwt filter

--- src/main/java/com/example/YerevanCinema/configs/security/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/YerevanCinema/configs/security/ApplicationSecurityConfiguration.java
@@ -1,34 +1,46 @@
 package com.example.YerevanCinema.configs.security;
 
+import com.example.YerevanCinema.filters.JwtAuthorizationFilter;
+import com.example.YerevanCinema.services.detailsServices.CustomerDetailsService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static com.example.YerevanCinema.enums.UserRole.ADMIN;
+import static com.example.YerevanCinema.enums.UserRole.CUSTOMER;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final CustomerDetailsService customerDetailsService;
+
+    public ApplicationSecurityConfiguration(CustomerDetailsService customerDetailsService) {
+        this.customerDetailsService = customerDetailsService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "/about", "/signup", "/contact", "/sessions", "/recover",
                         "/login*", "/recover/**", "/rest/api/**",
                         "/static/css/*", "/static/js/*", "/static/images/*", "/static/fonts/*", "/static/scss/*",
                         "/static/poppins/*", "/static/less/*").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login").permitAll()
-                .and()
-                .logout().permitAll()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID", "remember-me")
-                .logoutSuccessUrl("/");
+                .antMatchers("/admin*", "/admin/**").hasAuthority("ROLE_" + ADMIN.name())
+                .antMatchers("/customer*", "/customer/**").hasAuthority("ROLE_" + CUSTOMER.name())
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(customerDetailsService);
     }
 }

ad22ca7a5c29d568e47983ae122afb162ff21ee6
artapresyan <apresyanarturv@gmail.com>
2022-10-08 22:55:40 +0400

    implementing customer security config

--- src/main/java/com/example/YerevanCinema/configs/security/CustomerSecurityConfiguration.java
+++ src/main/java/com/example/YerevanCinema/configs/security/CustomerSecurityConfiguration.java
@@ -1,46 +1,58 @@
 package com.example.YerevanCinema.configs.security;
 
-import com.example.YerevanCinema.filters.JwtAuthorizationFilter;
+
 import com.example.YerevanCinema.services.detailsServices.CustomerDetailsService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static com.example.YerevanCinema.enums.UserRole.ADMIN;
 import static com.example.YerevanCinema.enums.UserRole.CUSTOMER;
 
 @Configuration
 @EnableWebSecurity
-public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Order(2)
+public class CustomerSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final CustomerDetailsService customerDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
-    public ApplicationSecurityConfiguration(CustomerDetailsService customerDetailsService) {
+    public CustomerSecurityConfiguration(CustomerDetailsService customerDetailsService, PasswordEncoder passwordEncoder) {
         this.customerDetailsService = customerDetailsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .addFilterAfter(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .antMatcher("/customer/**")
+                .antMatcher("/customer*")
                 .authorizeRequests()
                 .antMatchers("/", "/about", "/signup", "/contact", "/sessions", "/recover",
                         "/login*", "/recover/**", "/rest/api/**",
                         "/static/css/*", "/static/js/*", "/static/images/*", "/static/fonts/*", "/static/scss/*",
                         "/static/poppins/*", "/static/less/*").permitAll()
-                .antMatchers("/admin*", "/admin/**").hasAuthority("ROLE_" + ADMIN.name())
-                .antMatchers("/customer*", "/customer/**").hasAuthority("ROLE_" + CUSTOMER.name())
+                .antMatchers("/customer*", "/customer/**").hasAnyAuthority("ROLE_" + CUSTOMER.name())
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and()
+                .formLogin().loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/customer/")
+                .failureUrl("/login").permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .permitAll();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .userDetailsService(customerDetailsService);
+                .userDetailsService(customerDetailsService).passwordEncoder(passwordEncoder);
     }
 }

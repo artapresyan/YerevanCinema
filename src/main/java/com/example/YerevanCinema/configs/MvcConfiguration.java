6a702e0c23fe2a40f5359f6a82958b837f72f4c4
artapresyan <apresyanarturv@gmail.com>
2022-10-09 15:13:54 +0400

    added method to register login page view

--- src/main/java/com/example/YerevanCinema/configs/MvcConfiguration.java
+++ src/main/java/com/example/YerevanCinema/configs/MvcConfiguration.java
@@ -3,11 +3,18 @@ package com.example.YerevanCinema.configs;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry
+                .addViewController("/login").setViewName("login_view");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {

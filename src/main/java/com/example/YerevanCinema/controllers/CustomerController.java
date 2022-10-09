defe5d8158673903267b2361992878f1be71fb3f
artapresyan <apresyanarturv@gmail.com>
2022-10-10 00:13:14 +0400

    code refactoring and optimization

--- src/main/java/com/example/YerevanCinema/controllers/CustomerController.java
+++ src/main/java/com/example/YerevanCinema/controllers/CustomerController.java
@@ -3,6 +3,8 @@ package com.example.YerevanCinema.controllers;
 import com.example.YerevanCinema.entities.Customer;
 import com.example.YerevanCinema.entities.MovieSession;
 import com.example.YerevanCinema.entities.Ticket;
+import com.example.YerevanCinema.exceptions.HallNotFoundException;
+import com.example.YerevanCinema.exceptions.MovieNotFoundException;
 import com.example.YerevanCinema.exceptions.TicketNotFoundException;
 import com.example.YerevanCinema.exceptions.UserNotFoundException;
 import com.example.YerevanCinema.services.implementations.*;
@@ -48,161 +50,203 @@ public class CustomerController {
 
     @GetMapping
     public String getCustomerMainPage(HttpSession session, Model model) {
-        Customer customer = (Customer) session.getAttribute("user");
-        model.addAttribute("user", customer);
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
         return "customer_main_view";
     }
 
     @GetMapping("about")
     public String getAboutPage(HttpSession session, Model model) {
-        Customer customer = (Customer) session.getAttribute("user");
-        model.addAttribute("user", customer);
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
         return "customer_about_view";
     }
 
     @GetMapping("contact")
     public String getContactPage(HttpSession session, Model model) {
-        Customer customer = (Customer) session.getAttribute("user");
-        model.addAttribute("user", customer);
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
         return "customer_contact_view";
     }
 
     @PostMapping("contact_post")
     public String sendMessage(HttpSession session, @RequestParam("message") String message) {
         try {
-            Customer customer = (Customer) session.getAttribute("user");
+            Customer customer = (Customer) session.getAttribute("customer");
             gmailClientService.getSimpleMessage(customer.getCustomerEmail(), message);
         } catch (MessagingException e) {
-            return "customer_contact_view";
+            return "redirect:/customer/contact";
         }
-        return "customer_main_view";
+        return "redirect:/customer/";
     }
 
     @GetMapping("details")
     public String getAccountDetailsPage(HttpSession session, Model model) {
-        Customer customer = (Customer) session.getAttribute("user");
-        model.addAttribute("user", customer);
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
         return "customer_details_view";
     }
 
-    @DeleteMapping("details")
+    @PostMapping("details")
     public String removeAccount(HttpSession session, String password) {
-        Customer customer = (Customer) session.getAttribute("user");
+        Customer customer = (Customer) session.getAttribute("customer");
         customerService.selfRemoveCustomer(customer.getCustomerID(), password, passwordEncoder);
-        return "no_auth_main_view";
+        return "redirect:/";
     }
 
     @GetMapping("details/edit")
     public String getAccountDetailsEditPage(HttpSession session, Model model) {
-        Customer customer = (Customer) session.getAttribute("user");
-        model.addAttribute("user", customer);
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
         return "customer_details_edit_view";
     }
 
-    @PutMapping("details/edit_change")
+    @PostMapping("details/edit_change")
     public String updateAccountDetails(@RequestParam(value = "customer_name", required = false) String newName,
                                        @RequestParam(value = "customer_surname", required = false) String newSurname,
                                        @RequestParam(value = "customer_age", required = false) Integer newAge,
                                        @RequestParam(value = "customer_username", required = false) String newUsername,
                                        @RequestParam(value = "customer_email", required = false) String newEmail,
                                        @RequestParam(value = "new_password", required = false) String newPassword,
-                                       HttpSession session, Model model) {
-        Customer customer = (Customer) session.getAttribute("user");
-        customerService.updateCustomerData(customer.getCustomerID(), newName, newSurname, newAge, newUsername,
-                newEmail, customer.getCustomerPassword(), newPassword, customerValidationService, passwordEncoder);
+                                       @RequestParam(value = "new_password_confirm", required = false) String confirmNewPassword,
+                                       @RequestParam("password") String password, HttpSession session, Model model) {
+        Customer customer = (Customer) session.getAttribute("customer");
+        if (newPassword.equals(confirmNewPassword)) {
+            customerService.updateCustomerData(customer.getCustomerID(), newName, newSurname, newAge, newUsername,
+                    newEmail, password, newPassword, customerValidationService, passwordEncoder);
+        }
         try {
             customer = customerService.getCustomerByID(customer.getCustomerID());
-            session.setAttribute("user", customer);
-            model.addAttribute("user", customer);
-            return "customer_details_view";
+            session.setAttribute("customer", customer);
+            model.addAttribute("customer", customer);
+            return "redirect:/customer/details";
         } catch (UserNotFoundException e) {
-            return "customer_details_edit_view";
+            return "redirect:/customer/details/edit";
         }
     }
 
     @GetMapping("tickets")
     public String getPurchasedTickets(HttpSession session, Model model) {
-        Customer customer = (Customer) session.getAttribute("user");
-        model.addAttribute("tickets", customer.getCustomerTickets());
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
+        List<Ticket> tickets = ticketService.getAllTickets().stream()
+                .filter(ticket -> ticket.getCustomer().getCustomerID().equals(customer.getCustomerID()))
+                .collect(Collectors.toList());
+        model.addAttribute("tickets", tickets);
         return "customer_tickets_view";
     }
 
     @GetMapping("sessions")
     public String getSessionsPage(HttpSession session, Model model) {
-        Customer customer = (Customer) session.getAttribute("user");
-        List<MovieSession> sessions = movieSessionService.getAllMovieSessions().stream().filter(movieSession ->
-                        movieSession.getMovieSessionStart().isBefore(LocalDateTime.now().plusDays(14)))
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
+        List<MovieSession> sessions = movieSessionService.getAllMovieSessions().stream()
+                .filter(movieSession -> LocalDateTime.parse(movieSession.getMovieSessionStart())
+                        .isBefore(LocalDateTime.now().plusDays(14)))
                 .collect(Collectors.toList());
-        model.addAttribute("user", customer);
         model.addAttribute("sessions", sessions);
         return "customer_sessions_view";
     }
 
-    @GetMapping("sessions/movie")
-    public String getSessionsByMovieName(@RequestParam("movie_name") String name, Model model) {
-        List<MovieSession> movieSessions = movieSessionService.getAllMovieSessions().stream()
-                .filter(movieSession -> movieSession.getMovie().getMovieName().equals(name))
-                .collect(Collectors.toList());
-        model.addAttribute("movies", movieSessions);
-        return "customer_sessions_selected_view";
+    @PostMapping("sessions/selected")
+    public String getSessionsByMovieName(@RequestParam("key_value") String keyValue,
+                                         @RequestParam("selected") String selected, HttpSession session, Model model) {
+        Customer customer = (Customer) session.getAttribute("customer");
+        model.addAttribute("customer", customer);
+
+        try {
+            List<MovieSession> movieSessions = getSelectedMovieSessions(keyValue,selected);
+            model.addAttribute("selected_movie_sessions", movieSessions);
+            return "customer_sessions_selected_view";
+        } catch (MovieNotFoundException | HallNotFoundException e) {
+            return "redirect:/customer/sessions";
+        }
     }
 
-    @GetMapping("sessions/category")
-    public String getSessionsByMovieCategory(@RequestParam("movie_category") String movieCategory, Model model) {
-        List<MovieSession> movieSessions = movieSessionService.getAllMovieSessions().stream()
-                .filter(movieSession -> movieSession.getMovie().getMovieCategory().equals(movieCategory))
-                .collect(Collectors.toList());
-        model.addAttribute("movies", movieSessions);
-        return "customer_sessions_selected_view";
-    }
+//    @GetMapping("sessions/category")
+//    public String getSessionsByMovieCategory(@RequestParam("movie_category") String movieCategory, HttpSession session,
+//                                             Model model) {
+//        Customer customer = (Customer) session.getAttribute("customer");
+//        model.addAttribute("customer", customer);
+//        List<MovieSession> movieSessions = movieSessionService.getAllMovieSessions().stream()
+//                .filter(movieSession -> movieSession.getMovie().getMovieCategory().equals(movieCategory))
+//                .collect(Collectors.toList());
+//        model.addAttribute("movie_sessions_category", movieSessions);
+//        return "customer_sessions_category_selected_view";
+//    }
 
-    @GetMapping("sessions/start")
-    public String getSessionsByStart(@RequestParam("movie_start") LocalDateTime movieStart, Model model) {
-        List<MovieSession> movieSessions = movieSessionService.getAllMovieSessions().stream()
-                .filter(movieSession -> movieSession.getMovieSessionStart().isEqual(movieStart))
-                .collect(Collectors.toList());
-        model.addAttribute("movies", movieSessions);
-        return "customer_sessions_selected_view";
-    }
-
-    @GetMapping("sessions/hall")
-    public String getSessionsByHall(@RequestParam("movie_hall") Long hallID, Model model) {
-        List<MovieSession> movieSessions = movieSessionService.getAllMovieSessions().stream()
-                .filter(movieSession -> movieSession.getHall().getHallID().equals(hallID))
-                .collect(Collectors.toList());
-        model.addAttribute("movies", movieSessions);
-        return "customer_sessions_selected_view";
-    }
+//    @GetMapping("sessions/start")
+//    public String getSessionsByStart(@RequestParam("movie_start") String movieStart, HttpSession session, Model model) {
+//        Customer customer = (Customer) session.getAttribute("customer");
+//        model.addAttribute("customer", customer);
+//        List<MovieSession> movieSessions = movieSessionService.getAllMovieSessions().stream()
+//                .filter(movieSession -> movieSession.getMovieSessionStart().equals(movieStart))
+//                .collect(Collectors.toList());
+//        model.addAttribute("movie_sessions_start", movieSessions);
+//        return "customer_sessions_start_selected_view";
+//    }
+//
+//    @GetMapping("sessions/hall")
+//    public String getSessionsByHall(@RequestParam("movie_hall") Long hallID, HttpSession session, Model model) {
+//        Customer customer = (Customer) session.getAttribute("customer");
+//        model.addAttribute("customer", customer);
+//        List<MovieSession> movieSessions = movieSessionService.getAllMovieSessions().stream()
+//                .filter(movieSession -> movieSession.getHall().getHallID().equals(hallID))
+//                .collect(Collectors.toList());
+//        model.addAttribute("movie_sessions_hall", movieSessions);
+//        return "customer_sessions_hall_selected_view";
+//    }
 
     @PostMapping("sessions/purchase")
     public String sendTicketToCustomer(@RequestParam("selected_session_id") Long movieSessionID, HttpSession session) {
-        Customer customer = (Customer) session.getAttribute("user");
+        Customer customer = (Customer) session.getAttribute("customer");
         try {
             MovieSession movieSession = movieSessionService.getMovieSessionByID(movieSessionID);
             Ticket ticket = ticketService.addTicket(customer, movieSession);
             qrCodeService.generateQRCodeImage(customer);
             gmailClientService.sendMessageWithAttachment(customer, String.format(qrPath, ticket.getTicketID(),
                     customer.getCustomerUsername()));
-            return "customer_tickets_view";
+            return "redirect:/customer/tickets";
         } catch (Exception e) {
-            return "customer_sessions_view";
+            return "redirect:/customer/sessions";
         }
     }
 
-    @PostMapping("sessions/resend")
+    @PostMapping("tickets/resend")
     public String resendTicketToCustomer(@RequestParam("ticket_id") Long ticketID, HttpSession session) {
-        Customer customer = (Customer) session.getAttribute("user");
+        Customer customer = (Customer) session.getAttribute("customer");
         try {
             Ticket ticket = ticketService.getTicketByID(ticketID);
             if (customer.getCustomerID().equals(ticket.getCustomer().getCustomerID())) {
                 qrCodeService.generateQRCodeImage(customer);
                 gmailClientService.sendMessageWithAttachment(customer, String.format(qrPath, ticket.getTicketID(),
                         customer.getCustomerUsername()));
-                return "customer_tickets_view";
+                return "redirect:/customer/tickets";
             } else
                 throw new TicketNotFoundException("No Such Ticket");
         } catch (Exception e) {
-            return "customer_sessions_view";
+            return "redirect:/customer/";
         }
+    }
+
+    private List<MovieSession> getSelectedMovieSessions(String keyValue, String selected) throws MovieNotFoundException, HallNotFoundException {
+        if (keyValue.equalsIgnoreCase("Movie"))
+            return movieSessionService.getAllMovieSessions().stream()
+                    .filter(movieSession -> movieSession.getMovie().getMovieName().equals(selected))
+                    .collect(Collectors.toList());
+        else if (keyValue.equalsIgnoreCase("Category"))
+            return movieSessionService.getAllMovieSessions().stream()
+                    .filter(movieSession -> movieSession.getMovie().getMovieCategory().equals(selected))
+                    .collect(Collectors.toList());
+        else if (keyValue.equalsIgnoreCase("Price"))
+            return movieSessionService.getAllMovieSessions().stream()
+                    .filter(movieSession -> movieSession.getMovieSessionPrice().equals(Integer.parseInt(selected)))
+                    .collect(Collectors.toList());
+        else if (keyValue.equalsIgnoreCase("Hall"))
+            return movieSessionService.getAllMovieSessions().stream()
+                    .filter(movieSession -> movieSession.getHall().getHallName().equals(selected))
+                    .collect(Collectors.toList());
+        else
+            return List.of();
     }
 }
